4096736862862faa96a02faa3402b20b2e954e81
Sergey Igushkin <hotkeytlt@gmail.com>
2015-11-07 10:46:32 +0300

    Added UI.

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -13,16 +13,30 @@ public class Ui {
     private final JButton startButton = new JButton("Start");
     private final JButton stopButton = new JButton("Stop");
 
-    public Ui() {
+    private final Timer timer;
+
+    public Ui(Timer timer) {
+        this.timer = timer;
+
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setMinimumSize(new Dimension(300, 40));
+        frame.setMinimumSize(new Dimension(300, 100));
 
         frame.setContentPane(contentPanel);
         contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.LINE_AXIS));
 
-        frame.add(timeLabel);
         frame.add(startButton);
         frame.add(stopButton);
+        frame.add(timeLabel);
+
+        timer.stop();
+
+        startButton.addActionListener((actionEvent) -> {
+            timer.start();
+        });
+
+        stopButton.addActionListener((actionEvent) -> {
+            timer.stop();
+        });
     }
 
     public void show() {
